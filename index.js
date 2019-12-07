41f8a5275cd2138e3ab4f706b9a314b31e312ec8
stanjg <stan@gabes.nl>
2019-12-07 16:03:45 +0100

    Changed the way requestData is created

--- index.js
+++ index.js
@@ -697,21 +697,14 @@ const b2CloudStorage = class {
 		if(data.appendPath !== false){
 			apiUrl.pathname += `b2api/${this.version}/${data.url}`;
 		}
-		let requestData = {};
-        if (data.url.endsWith('b2_download_file_by_id')) {
-            requestData = _.defaults(data, {
-                method: 'get',
-                json: true,
-                headers: {},
-				encoding: null
-            });
-        } else {
-            let requestData = _.defaults(data, {
-                method: 'get',
-                json: true,
-                headers: {}
-            });
-		}
+		let requestData = _.defaults(data, {
+            method: 'get',
+            json: true,
+            headers: {}
+        });
+        if (data.url && data.url.endsWith('b2_download_file_by_id')) {
+            requestData.encoding = null;
+        }
 		requestData.url = apiUrl.toString();
 		// if auth data is set from `authorize` function and we haven't overridden it via `data.auth` or request headers, set it for this request
 		if(this.authData && !data.auth && !requestData.headers.Authorization){
@@ -727,7 +720,7 @@ const b2CloudStorage = class {
 				return callback(new Error('Auth token expired, and unable to re-authenticate to acquire new token.'));
 			}
 			reqCount++;
-			
+
 			return request(requestData, function(err, res, body){
                 if(err){
                     return callback(err);
